c9b2495266310c8035cf40feef5e7352cd589213
ansaralikunju <ansarpanoor311@gmail.com>
2023-09-22 20:25:36 +0530

    first commit

--- loop.js
+++ loop.js
@@ -1,4 +1,23 @@
-let a =6
-var b =10
-var sum = a+b
-console.log(sum)
+var read=require('readline-sync')
+var num=read.question('Enter A NumberS')
+var i,prime=0;
+if(num<=1)
+{
+    console.log("The Number is Not Prime")
+
+}
+else{
+    for(i=2;i<num;i++)
+    {
+        if(num%i==0){
+        prime=1;
+        break;
+        }
+    }
+    if(prime==0)
+    {
+        console.log("The Number is Prime")
+    }
+    else{
+        console.log("The Number is  Prime")
+    }
